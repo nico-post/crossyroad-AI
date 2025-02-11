cffde79e953566168e1fc94ab03bb20104eec5ad
nico-post <nicholas.post28@student.tamdistrict.org>
2025-02-11 21:54:04 +0000

    Co-authored-by: adamscheer4 <adamscheer4@users.noreply.github.com>

--- src/main.ts
+++ src/main.ts
@@ -28,3 +28,68 @@ const config: Types.Core.GameConfig = {
 };
 
 export default new Game(config);
+
+function preload ()
+{
+    this.load.image('sky', 'assets/sky.png');
+    this.load.image('ground', 'assets/platform.png');
+    this.load.image('star', 'assets/star.png');
+    this.load.image('bomb', 'assets/bomb.png');
+    this.load.spritesheet('dude', 
+        'assets/dude.png',
+        { frameWidth: 32, frameHeight: 48 }
+    );
+}
+
+function create ()
+{
+    this.add.image(400, 300, 'sky');
+    this.add.image(400, 300, 'star');
+}
+
+var platforms;
+
+function create ()
+{
+    this.add.image(400, 300, 'sky');
+
+    platforms = this.physics.add.staticGroup();
+
+    platforms.create(400, 568, 'ground').setScale(2).refreshBody();
+
+    platforms.create(600, 400, 'ground');
+    platforms.create(50, 250, 'ground');
+    platforms.create(750, 220, 'ground');
+}
+
+var config = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 300 },
+            debug: false
+        }
+    },
+    scene: {
+        preload: preload,
+        create: create,
+        update: update
+    }
+};
+
+platforms = this.physics.add.staticGroup();
+
+platforms.create(400, 568, 'ground').setScale(2).refreshBody();
+
+platforms.create(600, 400, 'ground');
+platforms.create(50, 250, 'ground');
+platforms.create(750, 220, 'ground');
+
+platforms.create(600, 400, 'ground');
+platforms.create(50, 250, 'ground');
+platforms.create(750, 220, 'ground');
+
+
--- src/scenes/Game.ts
+++ src/scenes/Game.ts
@@ -11,6 +11,11 @@ export class Game extends Scene
         super('Game');
     }
 
+preload(){
+    
+}
+
+
     create ()
     {
         this.camera = this.cameras.main;
@@ -32,4 +37,31 @@ export class Game extends Scene
 
         });
     }
+    
+}
+preload ()
+{
+    this.load.image('sky', 'assets/sky.png');
+    this.load.image('ground', 'assets/platform.png');
+    this.load.image('star', 'assets/star.png');
+    this.load.image('bomb', 'assets/bomb.png');
+    this.load.image('dude', 
+        'assets/dude.png',
+        { framewidth: 32, frameHeight: 48 }
+    );
+}
+
+create ()
+{
+    this.add.image(400, 300, 'sky');
+
+    this.platforms.create(400, 568, 'ground').setScale(2).refreshBody();
+    this.platforms.create(600, 400, 'ground');
+    this.platforms.create(50, 250, 'ground');
+    this.platforms.create(750, 220, 'ground');
+
+}
+
+update() {
+
 }
